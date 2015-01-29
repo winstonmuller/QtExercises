26efb028167cc69055ab7a18161cd790bbaa4a80
winstonmuller <winstonmuller@gmail.com>
2015-01-29 19:42:32 +1300

    Pointers Cheatsheet

--- Pointers/Pointers_Proj/main.cpp
+++ Pointers/Pointers_Proj/main.cpp
@@ -30,58 +30,52 @@ string Person::GetName()
 
 int main()
 {
-    
-/*
-     1. Pointer Declaration: * - when used in variable declaration, before variable name, indicates a pointer variable.	int* y;
-     2. Address of Operator: & - when used in assignment, before a variable name, returns the memory address of the variable	int* y = &x;
-     
-     3. Dereferencing Operator: * - when used in front of an existing pointer variable, returns the value of the memory location the pointer
-        points to. int pointer will return an int, MyClass will return MyClass etc. cout << *y; // outputs the value of y
-        
-     4. Member Access Operator Arrow: -> - Simplifies accessing of class components via pointer. Tom->GetName();
-     5. Null Pointer: p = 0 - Sets the value of p to the null pointer if p is a pointer type.
-     
-     6. Dynamic Variables: Variables that are created during program execution are called dynamic variables.
-     
-     7. new DataType; - allocate a single variable
-     8. new DataType[intExp]; allocate an array of variables
-*/
-    
     //  Declare x, a plain old variable
     int x = 100;
     
     cout << "x " << x << endl;  // Value of x is 100
 
-    //  We use * before a variable name to declare a pointer variable
-    int* y; // Declare a pointer variable, y (variable to store memory locations)
+    //  1. Pointer Declaration: * - when used in variable declaration, before variable name, indicates a pointer variable.
+    //  Declare a pointer variable, y (variable to store memory locations)
+    int* y; 
     
-    //  We can use the & character in front of a variable to get its memory address
-    y = &x; // Set y equal to memory location of x
+    //  2. Address of Operator: &
+    //  When used in assignment, before a variable name, returns the memory address of the variable
+    y = &x;
 
     // In this case, y is not an integer variable. Rather, it is a pointer to an integer variable 
     // The following line returns a memory address, not an integer value
     cout << "y " << y << endl;
+
+    //  3. Dereferencing Operator: * - when used in front of an existing pointer variable, returns the value of the memory location the pointer
+    //  points to. int pointer will return an int, MyClass will return MyClass etc. cout << *y; // outputs the value of y
     cout << "*y " << *y << endl;
-    
-	//	Because y refers to the memory location of x, changes to the content of y change x
+
+    //  The derefercing operator can also be used to change the data in the memory location.
 	*y = 45;
 	
 	cout << "*y " << *y << endl;
 	cout << "x " << x << endl;
 	
     //  Pointers and Classes
-    Person jim;
+    //  4. Dynamic Variables: Variables that are created during program execution are called dynamic variables.
+    //  Dynamic variables are created with the "new" keyword.
     
-    jim.SetName("Jim Johnson");
-    
-    cout << jim.GetName() << endl;
-    
+    //  5. new DataType; - allocate memory for a single variable
     Person *Tom = new Person();
+    
+    //  6. Member Access Operator Arrow: -> 
+    //  Simplifies accessing of class members via pointer.
+    //  Class members can also be access via the dereference operator but this is more clumsy because brackets are required
+    //  , i.e. (*Tom).GetName();
     Tom->SetName("Tom Tomoson");
     cout << "Address of Tom: " << Tom->GetName() << endl;
     
+    //  7. Null Pointer: p = 0 - Sets the value of p to the null pointer if p is a pointer type.
     Tom = 0;
-    cout << "Attempting to dereference null pointer" << endl;
+    cout << "Never attempt to dereference a null pointer" << endl;
+    
+    //  8. new DataType[intExp]; allocate an array of variables
     
     return 0;
 }
