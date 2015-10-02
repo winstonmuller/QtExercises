f38d34e5126df10d71013c34c5e6117756977eec
Winston Muller <winstonmuller@gmail.com>
2015-10-03 10:14:03 +1300

    Reflection working for one type

--- Reflection_GettingInfo/main.cpp
+++ Reflection_GettingInfo/main.cpp
@@ -2,6 +2,9 @@
 #include <QSpinBox>
 #include <QTextStream>
 #include <iostream>
+#include <QVariant>
+#include <QMetaObject>
+#include <QMetaProperty>
 
 void RunReflection(const QApplication &a)
 {
@@ -10,18 +13,32 @@ void RunReflection(const QApplication &a)
     QSpinBox* spinBox = new QSpinBox;
     const QMetaObject* metaSpin = spinBox->metaObject();
 
-    cout << "Winston was here" << endl;
+    cout << QString("Retrieving data for the class %1").arg(metaSpin->className()) << endl;
+    cout << QString("Number of methods: %1").arg(metaSpin->methodCount()) << endl;
 
     //  Show properties, their values (if QString), and all the method names
-    cout << "Retrieving data for the class " << metaSpin->className() << endl;
-    cout << metaSpin->methodCount() << endl;
+    for (int i = 0; i < metaSpin->propertyCount(); i++)
+    {
+        const QMetaProperty qpm = metaSpin->property(i);
+        cout << QString("Property : %1").arg(qpm.name()) << endl;
 
-    //  values
+        //  values that can be converted to a string
+        QVariant value = spinBox->property(qpm.name());
+        if (value.canConvert(QVariant::String))
+        {
+            cout << QString("The Property %1 has a string value of %2").arg(qpm.name()).arg(value.toString()) << endl;
+        }
+    }
 
     //  Method names
+    for (int c = 0; c < metaSpin->methodCount(); c++)
+    {
+        const QMetaMethod qmm = metaSpin->method(c);
+        cout << QString("Method Number %1 is %2 ").arg(QString::number(c)).arg(QString(qmm.name())) << endl;
+    }
 
+    cout << "Reflection Complete" << endl;
 }
-
 
 int main(int argc, char *argv[])
 {
